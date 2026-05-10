96298fd20f1ace31c1eabbd06543dd6868c2d543
shafaqataliaymin-lab <shafaqataliaymin@gmail.com>
2026-05-09 19:35:01 -0700

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,34 @@
+var CACHE_NAME = 'mastery-v1';
+var urlsToCache = ['/'];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
